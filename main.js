4120389c55f8519ba2e9cd2592357a676d53a571
ishi-ta-lal <ishita.lal01@gmail.com>
2024-12-03 17:35:45 +0530

    post-processing

--- main.js
+++ main.js
@@ -2,22 +2,23 @@ import './style.css';
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader.js';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
 import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
 import { RGBShiftShader } from 'three/examples/jsm/shaders/RGBShiftShader.js';
 import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass.js';
+import gsap from 'gsap';
 
 // Scene
 const scene = new THREE.Scene();
 // Camera
 const camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 0.1, 100);
-camera.position.z = 4;
+camera.position.z = 3.5;
 
 // renderer
 const renderer = new THREE.WebGLRenderer({ 
     canvas: document.querySelector('#canvas'),
     antialias: true, 
+    alpha: true,
 });
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -38,14 +39,12 @@ composer.addPass(rgbShiftPass);
 const pmremGenerator = new THREE.PMREMGenerator(renderer);
 pmremGenerator.compileEquirectangularShader();
 
-const controls = new OrbitControls(camera, renderer.domElement); 
-controls.enableDamping = true;
 let model;
 
 new RGBELoader()
     .load('https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/1k/metro_noord_1k.hdr', function(texture) {
         const envMap = pmremGenerator.fromEquirectangular(texture).texture;
-        scene.background = envMap;
+        // scene.background = envMap;
         scene.environment = envMap;
         
         texture.dispose();
@@ -68,10 +67,29 @@ new RGBELoader()
         );
     });
 
+window.addEventListener("mousemove", (e) => {
+    if (model) {
+        const rotationX = (e.clientX / window.innerWidth - .5) * (Math.PI * .12);
+        const rotationY = (e.clientY / window.innerHeight - .5) * (Math.PI * .12);
+        gsap.to(model.rotation, {
+            x: rotationY,
+            y: rotationX,
+            duration: 0.8,
+            ease: "power2.out"
+        });
+    }
+});
+
+window.addEventListener("resize", () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    composer.setSize(window.innerWidth, window.innerHeight);
+});
+
 // render
 function animate() {
     window.requestAnimationFrame(animate);
-    controls.update();
     composer.render(); // Using composer instead of renderer
 }
 animate();
