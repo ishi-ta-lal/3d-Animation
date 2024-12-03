ca6f85287047e2536bd77e9ae3b9a2158d895470
ishi-ta-lal <ishita.lal01@gmail.com>
2024-12-03 23:39:42 +0530

    final upload

--- main.js
+++ main.js
@@ -7,6 +7,9 @@ import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
 import { RGBShiftShader } from 'three/examples/jsm/shaders/RGBShiftShader.js';
 import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass.js';
 import gsap from 'gsap';
+import LocomotiveScroll from 'locomotive-scroll';
+
+const locomotiveScroll = new LocomotiveScroll();
 
 // Scene
 const scene = new THREE.Scene();
